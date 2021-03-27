6fbd36a53727b2f54cc842a1e8018d2be590766f
rameshwarmali7 <55401165+rameshwarmali7@users.noreply.github.com>
2021-03-27 13:37:34 -0700

    Created Stack Using LinkedList

---
+++ Stack/StackUsingLinkedList/src/StackUsingLinkedListMain.java
@@ -0,0 +1,90 @@
+class Node {
+	
+	public int data;
+	public Node nextNode;
+	
+	public Node(int data) {
+		this.data = data;
+	}
+}
+
+class Stack {
+	
+	private int size;
+	private Node top;
+	
+	public Stack() {}
+	
+	public boolean push(int data) {
+		
+		Node givenNode = new Node(data);
+		
+		if(!isEmpty()) givenNode.nextNode = this.top;
+		
+		this.top = givenNode;
+		
+		this.size++;
+		printStack();
+		return true;
+	}
+	
+	public int pop() {
+		
+		if(isEmpty()) throw new IllegalStateException();
+		
+		int data = this.top.data;
+		this.top = this.top.nextNode;
+		
+		this.size--;
+		printStack();
+		return data;
+	}
+	
+	public int peek() {
+		
+		if(isEmpty()) throw new IllegalStateException();
+		
+		return this.top.data;
+	}
+	
+	public boolean isEmpty() {
+		
+		return this.size == 0;
+	}
+	
+	public int size() {
+		
+		return this.size;
+	}
+	
+	private void printStack() {
+		
+		if(isEmpty()) {
+			System.out.println("Stack is Empty");
+			return;
+		}
+		
+		Node currentNode = this.top;
+		
+		System.out.print("Size=" + this.size + " Top=" + this.top.data + " Stack=");
+		while(currentNode != null) {
+			System.out.print(currentNode.data + " ");
+			currentNode = currentNode.nextNode;
+		}
+		System.out.println();
+	}
+}
+
+public class StackUsingLinkedListMain {
+	
+	public static void main(String[] args) {
+		
+		Stack s = new Stack();
+		
+		System.out.println(s.isEmpty());
+		
+		s.push(23); s.push(54);
+		
+		s.pop(); s.pop(); s.pop();
+	}
+}
