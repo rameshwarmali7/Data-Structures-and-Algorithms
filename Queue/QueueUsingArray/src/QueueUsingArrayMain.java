aeaf53ea1f0e782a91864f304fafe150e6c5c8c5
rameshwarmali7 <55401165+rameshwarmali7@users.noreply.github.com>
2021-03-30 17:21:20 -0700

    Update 03

--- Queue/QueueUsingArray/src/QueueUsingArrayMain.java
+++ Queue/QueueUsingArray/src/QueueUsingArrayMain.java
@@ -9,23 +9,19 @@ class Queue {
 	public Queue(int capacity) {
 		
 		this.capacity = capacity;
-		this.front = -1;
-		this.rear = -1;
-		this.queue = new int[this.capacity];
+		this.front = 0;
+		this.rear = 0;
+		this.queue = new int[capacity];
 		printQueue();
 	}
 	
 	public boolean add(int data) {
 		
 		if(this.size == this.capacity) throw new IllegalStateException();
-		else if (isEmpty()) {
-			this.rear = 0;
-			this.front = 0;
-		} else {
-			this.rear = (this.rear + 1) % this.capacity;
-		}
 		
-		queue[this.rear] = data;
+		if(!isEmpty()) this.rear = (this.rear + 1) % this.capacity;
+		
+		this.queue[this.rear] = data;
 		this.size++;
 		printQueue();
 		return true;
@@ -33,17 +29,13 @@ class Queue {
 	
 	public int remove() {
 		
-		int result;
-		
 		if(isEmpty()) throw new IllegalStateException();
-		else if(this.size == 1) {
-			result = queue[this.front];
-			this.rear = -1;
-			this.front = -1;
-		} else {
-			result = queue[this.front];
-			this.front = (this.front + 1) % this.capacity;
-		}
+		
+		int result = this.queue[this.front];
+		
+		this.front = (this.front + 1) % this.capacity;
+		
+		if(this.size == 1) this.rear = this.front;
 		
 		this.size--;
 		printQueue();
@@ -52,10 +44,8 @@ class Queue {
 	
 	public int peek() {
 		
-		int result;
-		if(isEmpty()) throw new IllegalStateException();
-		else result = queue[this.front];
-		return result;
+		if(isEmpty()) throw new IllegalStateException(); 
+		return queue[this.front];
 	}
 	
 	public int size() {
@@ -69,13 +59,18 @@ class Queue {
 	}
 	
 	private void printQueue() {
+		
+		if(isEmpty()) System.out.print("Size=0 Front=0 Rear=0 List=");
+		else System.out.print("Size=" + this.size + " Front=" + this.queue[this.front] + " Rear=" + this.queue[this.rear] + " List=");
 
-		if(isEmpty()) System.out.println("Queue is Empty");
-		else {
-			System.out.print("Size=" + this.size + " Front=" + this.queue[this.front] + " Rear=" + this.queue[this.rear] + " List=");
-			for(int i = 0; i < this.capacity; i++) System.out.print(this.queue[i] + " ");
-			System.out.println();
+		int i = this.front;
+		int count = 0;
+		while(count<this.size) {
+			System.out.print(this.queue[i] + " ");
+			i = (i + this.capacity + 1) % this.capacity;
+			count++;
 		}
+		System.out.println();
 	}
 }
 
@@ -83,14 +78,22 @@ public class QueueUsingArrayMain {
 	
 	public static void main(String[] args) {
 		
-		Queue q = new Queue(4);
+		Queue q = new Queue(4); System.out.println();
 		
-		q.add(52);q.add(23);q.add(31);q.add(89);
+		q.add(5); q.add(3); q.add(1); q.add(9); System.out.println();
+		
+		q.remove(); System.out.println();
+		
+		q.add(2); System.out.println();
+		
+		q.remove(); System.out.println();
+		
+		System.out.println(q.peek()); System.out.println();
+		
+		q.add(4); System.out.println();
+		
+		q.remove(); q.remove(); q.remove(); q.remove(); System.out.println();
 		
 		q.remove();
-		System.out.println(q.peek());
-		
-		q.add(45);
-		q.remove();q.remove();q.remove();q.remove();q.remove();
 	}
 }
