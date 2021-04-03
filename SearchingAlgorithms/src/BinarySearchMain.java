24ba64c68b356edf6ac6c634c533b53d413ba30e
rameshwarmali7 <55401165+rameshwarmali7@users.noreply.github.com>
2021-04-02 23:25:03 -0700

    Update 01

--- SearchingAlgorithms/src/BinarySearchMain.java
+++ SearchingAlgorithms/src/BinarySearchMain.java
@@ -5,14 +5,13 @@ public class BinarySearchMain {
 	
 	public static void main(String[] args) {
 		
-		List<Integer> givenArray = Arrays.asList(3, 4, 7, 11, 45, 76);
+		final List<Integer> givenList = Arrays.asList(3, 7, 7, 7, 7, 11, 45, 45, 76);
 		
-		System.out.println(binarySearchIterative(givenArray, 7));
-		System.out.println(binarySearchRecursive(givenArray, 0, givenArray.size()-1, 7));
-		
+		System.out.println(binarySearchIterative(givenList, 45));
+		System.out.println(binarySearchRecursive(givenList, 45));
 	}
 	
-	public static int binarySearchIterative(List<Integer> givenList, int searchValue) {
+	public static int binarySearchIterative(final List<Integer> givenList, final int searchValue) {
 		
 		int midIndex = 0, left = 0, right = givenList.size() - 1;
 		
@@ -20,25 +19,42 @@ public class BinarySearchMain {
 			
 			midIndex = left+(right-left)/2;
 			
-			if(givenList.get(midIndex) == searchValue) return midIndex;
+			if(givenList.get(midIndex) == searchValue) break;
 			
 			if(givenList.get(midIndex) < searchValue) left = midIndex + 1;
 			else right = midIndex - 1;
 		}
 		
-		return -1;
+		return getFirstIndexOfElement(givenList, searchValue, midIndex);
 	}
 	
-	public static int binarySearchRecursive(List<Integer> givenList, int left, int right, int x) {
+	public static int binarySearchRecursive(final List<Integer> givenList, final int searchValue) {
+		
+		int index = binarySearchRecursive(givenList, searchValue, 0, givenList.size()-1);
+		
+		return getFirstIndexOfElement(givenList, searchValue, index);
+	}
+	
+	public static int binarySearchRecursive(final List<Integer> givenList, final int searchValue, int left, int right) {
 		
 		if(left <= right) {
 			
 			int midIndex = left + (right-left)/2;
 			
-			if(givenList.get(midIndex) == x) return midIndex;
+			if(givenList.get(midIndex) == searchValue) return midIndex;
 			
-			if(givenList.get(midIndex) > x) return binarySearchRecursive(givenList, left, midIndex-1, x);
-			else return binarySearchRecursive(givenList, midIndex+1, right, x);
+			if(givenList.get(midIndex) > searchValue) return binarySearchRecursive(givenList, searchValue, left, midIndex-1);
+			else return binarySearchRecursive(givenList, searchValue, midIndex+1, right);
+		}
+		
+		return -1;
+	}
+	
+	public static int getFirstIndexOfElement(final List<Integer> givenList, final int searchValue, int index) {
+		
+		if(givenList.get(index) == searchValue) {
+			while((index-1) >= 0 && givenList.get(index-1) == searchValue) index--;
+			return index;
 		}
 		
 		return -1;
