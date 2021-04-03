23c587bfb0b1dea9b4f523a778a27e7e59865bcb
rameshwarmali7 <55401165+rameshwarmali7@users.noreply.github.com>
2021-04-03 13:03:25 -0700

    QuickSort

---
+++ SortingAlgorithms/src/QuickSortMain.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.Random;
+
+class QuickSort {
+	
+	public int[] quickSort(final int[] givenArray) {
+		
+		return quickSort(givenArray, 0, givenArray.length - 1);
+	}
+	
+	private int[] quickSort(final int[] givenArray, final int left, final int right) {
+		
+		if(left < right) {
+			final int partition = partition(givenArray, left, right);
+			
+			quickSort(givenArray, left, partition-1);
+			quickSort(givenArray, partition+1, right);
+		}
+		
+		return givenArray;
+	}
+	
+	private int[] swap(final int[] givenArray, final int index1, final int index2) {
+		
+		final int temp = givenArray[index1];
+		givenArray[index1] = givenArray[index2];
+		givenArray[index2] = temp;
+		
+		return givenArray;
+	}
+	
+	private int getRandomPivot(final int leftIndex, final int rightIndex) {
+		//Check document -> https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+		Random random = new Random();
+		
+		return random.nextInt((rightIndex-leftIndex)+1)+leftIndex;
+	}
+	
+	private int partition(final int[] givenArray, int leftIndex, int rightIndex) {
+		
+		final int pivotIndex = getRandomPivot(leftIndex, rightIndex);
+		swap(givenArray, leftIndex, pivotIndex);
+		
+		int border = leftIndex + 1;
+		
+		for(int i = border; i <= rightIndex; i++) {
+			if(givenArray[i] < givenArray[leftIndex]) swap(givenArray, i, border++); 
+		}
+		
+		swap(givenArray, leftIndex, border-1);
+		return border-1;
+	}
+}
+
+public class QuickSortMain {
+	
+	public static void main(String[] args) {
+		
+		int[] givenArray = new int[] {2, 5, 44, 23, 85, 98, 20, 12, 85, 44, 30};
+		
+		QuickSort qs = new QuickSort();
+		
+		System.out.println(Arrays.toString(givenArray));
+		System.out.println(Arrays.toString(qs.quickSort(givenArray)));
+	}
+}
