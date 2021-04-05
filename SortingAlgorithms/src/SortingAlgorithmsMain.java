ea5d2b2c4a413df77b09903310775281bf97acc1
rameshwarmali7 <55401165+rameshwarmali7@users.noreply.github.com>
2021-04-05 09:43:04 -0700

    Update 01

--- SortingAlgorithms/src/SortingAlgorithmsMain.java
+++ SortingAlgorithms/src/SortingAlgorithmsMain.java
@@ -1,62 +1,104 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class SortingAlgorithmsMain {
 	
 	public static void main(String[] args) {
 		
-		final List<Integer> givenArray = Arrays.asList(6, 5, 3, 4, 3, 2, 6, 1);
-		System.out.println("givenArray=" + givenArray);
+		final List<Integer> givenList = new ArrayList<Integer>();
+
+		for(int i = 0; i < 10; i++) {
+			int randomNum =(int) (Math.random() * 10 + 0);
+			givenList.add(randomNum);
+		}
 		
-		insertionSort(givenArray);
-		bubbleSort(givenArray);
-		selectionSort(givenArray);
+		System.out.println("givenList=" + givenList);
+		insertionSort(givenList);
+		bubbleSort(givenList);
+		selectionSort(givenList);
+		countSort(givenList);
 	}
 	
-	public static List<Integer> insertionSort(final List<Integer> givenArray) {
+	public static List<Integer> insertionSort(final List<Integer> givenList) {
 		
-		for(int i = 1; i < givenArray.size(); i++) {
-			
+		for(int i = 1; i < givenList.size(); i++) {
 			int j = i;
 			while(j > 0) {
-				if(givenArray.get(j) < givenArray.get(j-1)) Collections.swap(givenArray, j, j-1);
+				if(givenList.get(j) < givenList.get(j-1)) Collections.swap(givenList, j, j-1);
 				j--;
 			}
 		}
 		
-		System.out.println("insertionSort=" + givenArray);
-		return givenArray;
+		System.out.println("insertion=" + givenList);
+		return givenList;
 	}
 	
-	public static List<Integer> bubbleSort(final List<Integer> givenArray) {
+	public static List<Integer> bubbleSort(final List<Integer> givenList) {
 		
-		for(int i = 0; i< givenArray.size(); i++) {
-			for(int j = 0; j < givenArray.size() - 1 - i; j++) {
-				
-				if(givenArray.get(j) > givenArray.get(j+1)) Collections.swap(givenArray, j, j+1);
+		for(int i = 0; i< givenList.size(); i++) {
+			for(int j = 0; j < givenList.size() - 1 - i; j++) {
+				if(givenList.get(j) > givenList.get(j+1)) Collections.swap(givenList, j, j+1);
 			}
 		}
 		
-		System.out.println("bubbleSort=" + givenArray);
-		return givenArray;
+		System.out.println("bubbleSor=" + givenList);
+		return givenList;
 	}
 	
-	public static List<Integer> selectionSort(final List<Integer> givenArray) {
+	public static List<Integer> selectionSort(final List<Integer> givenList) {
 		
-		for(int i = 0; i < givenArray.size(); i++) {
-			
+		for(int i = 0; i < givenList.size(); i++) {
 			int minimumIndex = i;
 			
-			for(int j = i + 1; j < givenArray.size(); j++) {
-				
-				if(givenArray.get(j) < givenArray.get(minimumIndex)) minimumIndex = j;
+			for(int j = i + 1; j < givenList.size(); j++) {
+				if(givenList.get(j) < givenList.get(minimumIndex)) minimumIndex = j;
 			}
 			
-			Collections.swap(givenArray, i, minimumIndex);
+			Collections.swap(givenList, i, minimumIndex);
 		}
 		
-		System.out.println("selectionSort=" + givenArray);
-		return givenArray;
+		System.out.println("selection=" + givenList);
+		return givenList;
+	}
+	
+	public static List<Integer> countSort(final List<Integer> givenList) {
+		
+		int minimumValue = Integer.MAX_VALUE;
+		int maximumValue = Integer.MIN_VALUE;
+		
+		for(int num : givenList) {
+			minimumValue = Math.min(minimumValue, num);
+			maximumValue = Math.max(maximumValue, num);
+		}
+		
+		final List<Integer> countList = new ArrayList<Integer>();
+		
+		for(int i = 0; i < maximumValue - minimumValue + 1; i++) countList.add(0);
+		
+		for(int num : givenList) {
+			final int indexOfCountList = num-minimumValue;
+			int count = countList.get(indexOfCountList) + 1;
+			countList.remove(indexOfCountList);
+			countList.add(indexOfCountList, count);
+		}
+		
+		int indexOfGivenList = 0;
+		
+		for(int i = 0; i < countList.size(); i++) {
+			int count = countList.get(i);
+			while(count > 0) {
+				int num = i + minimumValue;
+				givenList.remove(indexOfGivenList);
+				givenList.add(indexOfGivenList,num);
+				indexOfGivenList++;
+				count--;
+			}
+		}
+
+		System.out.println("countSort=" + givenList);
+		return givenList;
 	}
 }
